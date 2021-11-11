f841a4677b6f7aa293bd17e4fd678464475f0084
kernelland <kernelland@protonmail.ch>
2021-11-11 11:46:48 +0100

    Fixed symlink creation

--- remarkable_files_to_real_files.py
+++ remarkable_files_to_real_files.py
@@ -1,7 +1,6 @@
 from RemarkableFlatFileSystem import RemarkableFlatFileSystem
 from distutils.dir_util import mkpath
 from os import path, symlink
-from time import sleep
 
 IN_PATH = "~/remarkable-files-to-real-files/data/remarkable/xochitl/"
 OUT_PATH = "~/remarkable-files-to-real-files/syms/"
@@ -13,19 +12,22 @@ def create_dir_if_not_exists(dir):
 
 
 def create_symlink_for_rm_file(rm_file):
-    if not path.exists(rm_file.real_file_path):
-        symlink(
-            rm_file.real_file_path,
-            rm_file.rm_file_path
-        )
+    if not path.exists(rm_file.rm_file_path) \
+            and bool(rm_file.real_file_path):
+        try:
+            symlink(rm_file.real_file_path, rm_file.rm_file_path)
+            print(f"└─> Symlink success!\n")
+        except FileNotFoundError:
+            print(f"└─> Symlink failed\n")
+    else:
+        print(f"└─> File already exists\n")
 
 
 if __name__ == "__main__":
     rm_files = RemarkableFlatFileSystem(IN_PATH, OUT_PATH)
     for file in rm_files:
-        if file.rm_parent_path:
-            print("├─ Trying to create symlink")
-            print(f"├─> FROM: {file.real_file_path}")
-            print(f"└─> TO: {file.rm_file_path}\n")
-            create_dir_if_not_exists(file.rm_parent_path)
-            create_symlink_for_rm_file(file)
+        print("├─ Trying to create symlink")
+        print(f"├─> FROM: {file.real_file_path}")
+        print(f"├─> TO: {file.rm_file_path}")
+        create_dir_if_not_exists(file.rm_parent_path)
+        create_symlink_for_rm_file(file)
