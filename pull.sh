da876606977e24ad1ac90e4d44c052fd261136c2
bufferbandit <kernelland@protonmail.ch>
2022-03-09 14:54:12 +0100

    changed update script

--- pull.sh
+++
@@ -1,2 +0,0 @@
-# Git pull doesn't work that well on the remarkable (ssl error or something). So download a zip and unzip it
-rm -rf r2f && wget -qO- https://github.com/bufferbandit/remarkable-files-to-real-files/archive/refs/heads/master.zip | busybox unzip - && mv remarkable-files-to-real-files-master r2f
